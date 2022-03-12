35bd684111dac8d19a51441ac349a1e77c7bcacb
Jing Li <jingliapply234@yahoo.com>
2022-03-12 15:55:32 +0800

    Initial Commit

---
+++ DrinkMixer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkMixer
+//
+//  Created by Jing Li on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
