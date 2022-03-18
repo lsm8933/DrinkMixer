200d9ae89dafecea216a125245dc3eebcccf3c9d
Jing Li <jingliapply234@yahoo.com>
2022-03-18 22:49:47 +0800

    add sectionHeader, adjust layout of collectionView (section top and bottom contentInsets).

--- DrinkMixer/Controllers/HomeViewController.swift
+++ DrinkMixer/Controllers/HomeViewController.swift
@@ -9,19 +9,23 @@ import UIKit
 
 class HomeViewController: UICollectionViewController {
     
+    private let sectionHeaderId = "sectionHeaderId"
     private let smallCellId = "smallCellId"
     
     init() {
         let layout = UICollectionViewCompositionalLayout { sectionNumber, _ in
 
             let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
-            item.contentInsets = .init(top: 0, leading: 0, bottom: 8, trailing: 8)
+            item.contentInsets.trailing = 8
             
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.4), heightDimension: .absolute(200)), subitems: [item])
             
             let section = NSCollectionLayoutSection(group: group)
             section.orthogonalScrollingBehavior = .groupPaging
-            section.contentInsets.leading = 16
+            section.contentInsets = .init(top: 16, leading: 16, bottom: 32, trailing: 0)
+            
+            let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .estimated(30)), elementKind: UICollectionView.elementKindSectionHeader, alignment: .topLeading)
+            section.boundarySupplementaryItems = [sectionHeader]
             
             return section
         }
@@ -35,16 +39,23 @@ class HomeViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //collectionView.backgroundColor = .cyan
         
+        collectionView.register(HomeCategoriesSectionHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: sectionHeaderId)
         collectionView.register(DrinkSmallCell.self, forCellWithReuseIdentifier: smallCellId)
     }
     
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return 4
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: sectionHeaderId, for: indexPath)
+        return header
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -57,5 +68,33 @@ class HomeViewController: UICollectionViewController {
 class DrinkSmallCell: BaseCell {
     override func setupViews() {
         backgroundColor = .red
+    }
+}
+
+class HomeCategoriesSectionHeader: UICollectionReusableView {
+    let categoryNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Category"
+        label.font = .boldSystemFont(ofSize: 26)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //backgroundColor = .magenta
+        
+        categoryNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(categoryNameLabel)
+        
+        NSLayoutConstraint.activate([
+            categoryNameLabel.topAnchor.constraint(equalTo: topAnchor),
+            categoryNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            categoryNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            categoryNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
